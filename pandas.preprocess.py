d57cef4d6a01df6c3f7e0954e1401abfe957239f
vancodocton <duongtruong.nguyenky@gmail.com>
2021-06-19 21:21:17 +0700

    update preprocessing script

--- pandas.preprocess.py
+++ pandas.preprocess.py
@@ -1,44 +1,34 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
+# import library
 import pandas
+import numpy
 
 
 # %%
 # assign directory
 dir_name = './datasets/'
-file_name = 'onuni.Business.csv'
+file_name = 'preprocessed.onuni.FinanceAccounting.csv'
 
-
-# %%
 # load dataframe
-dir_name = './datasets/'
-file_name = 'onuni.Business.csv'
-
-coursesinfo_df = pandas.read_csv(dir_name + file_name, encoding="utf-8", nrows=None)
-coursesinfo_df.info()
+df = pandas.read_csv(dir_name + file_name, encoding="utf-8", nrows=None)
 
 
 # %%
-# drop unused columns
-unusedcols = ['is_wishlisted',
-            'price_detail__currency',
-            'discount_price__currency',
-            'price_detail__price_string',
-            'discount_price__price_string',
-            'rating']
-coursesinfo_df = coursesinfo_df.drop(unusedcols, axis=1)
-coursesinfo_df.info()
+# check the inputted dataframe
+df.head()
 
 
 # %%
-# replace null data in 'price_detail__amount' and 'discount_price__amount'
-coursesinfo_df['price_detail__amount'] = coursesinfo_df['price_detail__amount'].apply(lambda x: 0 if pandas.isnull(x) else x) 
-coursesinfo_df['discount_price__amount'] = coursesinfo_df['discount_price__amount'].apply(lambda x: 0 if pandas.isnull(x) else x)
-coursesinfo_df.info()
+df.hist(figsize=(25,15))
 
 
 # %%
-coursesinfo_df.to_csv(dir_name + 'preprocessed.' + file_name, encoding="utf-8")
+df.groupby('discount').mean()['rating'].plot(x='discount', y='rating', kind='line', figsize=(40,10))
+
+
+# %%
+df.groupby('discount_percent')['rating'].mean().plot(x='rating', y='discount_percent', figsize =(30,10))
 
 
